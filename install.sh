df10b5ee2baea459b96694d1c9910c0ff3dd05ed
Zachariah Gibbens <zach@gibbens.dev>
2020-12-17 13:50:34 -0400

    init

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 mkdir -p $HOME/.ssh
 sudo apt -y install curl
 curl https://github.com/zachgibbens.keys >> $HOME/.ssh/authorized_keys
-sudo apt -y install ssh screen mosh bind9
+sudo apt -y install ssh screen mosh bind9 dnsutils
+sudo cp ./named.conf.opennic /etc/bind/named.conf.opennic
