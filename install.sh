915419d88b89e6e22ff485800a5907b02da03367
Zachariah Gibbens <zach@gibbens.dev>
2021-04-12 20:21:31 -0300

    modified:   authorized_keys 	modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir -p $HOME/.ssh
 sudo apt -y install curl
+cp $HOME/.ssh/authorized_keys $HOME/.ssh/authorized_keys.backup_$(date -u +%Y-%m-%d_%H:%M:%S)
 curl https://github.com/zachgibbens.keys >> $HOME/.ssh/authorized_keys
 sudo apt -y install ssh screen mosh bind9 dnsutils
 sudo cp ./named.conf.opennic /etc/bind/named.conf.opennic
